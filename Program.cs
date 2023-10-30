a9a7e816f313fd464f4d6eecd95d871053f0a8b7
lutsenkosasha <128168000+lutsenkosasha@users.noreply.github.com>
2023-10-30 07:34:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using FoodDelivery.WebAPI.IoC;
+using FoodDelivery.WebAPI.Settings;
+using FoodDelivery.WebAPI.IoC;
+
+var configuration = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json", optional: false)
+.Build();
+
+var settings = FoodDeliverySettingsReader.Read(configuration);
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+SerilogConfigurator.ConfigureService(builder);
+SwaggerConfigurator.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+SerilogConfigurator.ConfigureApplication(app);
+SwaggerConfigurator.ConfigureApplication(app);
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
