d33612fe844e83bff307a26390d08c90ef89917a
lutsenkosasha <128168000+lutsenkosasha@users.noreply.github.com>
2023-10-30 04:13:59 +0300

    Update README.md

---
+++ FoodDelivery.WebAPI/Settings/FoodDeliverySettingsReader.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodDelivery.WebAPI.Settings
+{
+    public static class FoodDeliverySettingsReader
+    {
+        public static FoodDeliverySettings Read(IConfiguration configuration)
+        {
+
+            return new FoodDeliverySettings();
+        }
+    }
+}
